385fa665050f5787efbf079fef0ad9b320c469bc
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-08 17:01:31 +0100

    inserito programma per parola palindroma

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -2,5 +2,14 @@
 Creare una funzione per capire se la parola inserita è palindroma */
 // 1. inizializzo le variabili
 var parolaUtente = prompt("Inserisci una parola");
-var parolaPalindroma = [];
 // 2. scrivo la funzione per verificare se la parola è palindorma
+function palindroma (parola){
+  if (parola == parola.split("").reverse().join("")){
+    return "La parola è palindroma";
+  } else {
+    return "La parola non è palindroma";
+  }
+}
+// 3. stampo in console se la parola è palindroma o meno
+var palindroma = palindroma(parolaUtente);
+console.log(palindroma);
